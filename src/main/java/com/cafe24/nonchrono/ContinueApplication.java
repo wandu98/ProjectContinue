1cd2efdba623d57f5a1e3dc8cb41a8e0ef618100
Nonchrono <koochunwoo@gmail.com>
2022-11-29 16:59:57 +0900

    모집 게시판 1차 수정

--- src/main/java/com/cafe24/nonchrono/ContinueApplication.java
+++ src/main/java/com/cafe24/nonchrono/ContinueApplication.java
@@ -1,7 +1,15 @@
 package com.cafe24.nonchrono;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class ContinueApplication {
