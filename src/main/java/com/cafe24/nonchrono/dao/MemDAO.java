73f2fba9a08785ebad914b7755d4065ffe81cee5
KHS <khs7632@naver.com>
2022-12-01 18:13:05 +0900

    2022.12.01 wishlist, 쿠폰적립금 view

--- src/main/java/com/cafe24/nonchrono/dao/MemDAO.java
+++ src/main/java/com/cafe24/nonchrono/dao/MemDAO.java
@@ -2,10 +2,12 @@ package com.cafe24.nonchrono.dao;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.lang.reflect.Member;
 import java.util.List;
 
+@Repository
 public class MemDAO { //Data Access Object
 
     @Autowired
