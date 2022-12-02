f0000880745918a6d9872f4794ca03fae628a10a
KHS <khs7632@naver.com>
2022-12-02 09:31:49 +0900

    2022.12.02

--- src/main/resources/static/sql/project_table.sql
+++ src/main/resources/static/sql/project_table.sql
@@ -279,7 +279,7 @@ CREATE TABLE tb_mem
     ,
     buyer_bad   int          NOT NULL default 0         -- 누적신고횟수
     ,
-    mem_pic    varchar      NOT NULL                    -- 프로필사진
+    mem_pic    varchar(255)      NOT NULL               -- 프로필사진
     ,
     mem_joindate date       NOT NULL  default now()
 );
