f7440e14c2858ba9154bb70a353bab0f20afc4ce
Nonchrono <koochunwoo@gmail.com>
2022-12-13 09:37:23 +0900

    (천우) 더보기 버튼 수정 중 (MoreDTO 추가)

---
+++ src/main/java/com/cafe24/nonchrono/dto/MoreDTO.java
@@ -0,0 +1,20 @@
+package com.cafe24.nonchrono.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MoreDTO {
+
+    private int rcrbrd_num;
+    private String gm_code;
+    private String rcrbrd_edate;
+    private String rcrbrd_subject;
+    private String gm_name;
+    private int count;
+    private int rcrbrd_max;
+
+}
